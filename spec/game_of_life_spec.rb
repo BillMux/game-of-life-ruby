fdc66f757eef0fa59b03435edc6a366e472ae9ca
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-26 14:15:45 +0000

    First test, initialisation passing

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -2,7 +2,21 @@ require 'rspec'
 require_relative '../lib/game_of_life.rb'
 
 describe GameOfLife do
-  it 'is' do
-    expect(subject).to be
+
+  it 'begins with a 10x10 world' do
+    expect(subject.world).to eq(
+      [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+      ]
+    )
   end
 end
