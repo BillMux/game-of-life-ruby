9a35847a8f8ad5b54502dbda4f3bb7be3f059004
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-26 16:27:48 +0000

    Replace response test with one line block

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -1,12 +1,18 @@
 require 'rspec'
 require_relative '../lib/world.rb'
+require_relative '../lib/cell.rb'
 
 describe World do
-  it 'should respond to methods' do
-    expect(subject).to respond_to(:rows, :cols, :grid)
-  end
+  let(:cell) { double :cell }
 
-  it 'begins with a 20x20 world' do
-    expect(subject.grid).to eq(Array.new(20){ Array.new(20, 1) })
+  it { is_expected.to respond_to :rows, :cols, :grid }
+
+  it 'begins with a 20x20 cell grid' do
+    subject.grid.each do |row|
+      expect(row).to be_an_instance_of Array
+      row.each do |col|
+        expect(col).to be_an_instance_of Cell
+      end
+    end
   end
 end
