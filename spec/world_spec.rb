4db16cd6d69c510fa439448ac3487446edc330a5
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-26 14:59:37 +0000

    Test attribute readers

--- spec/world_spec.rb
+++ spec/world_spec.rb
@@ -2,6 +2,9 @@ require 'rspec'
 require_relative '../lib/world.rb'
 
 describe World do
+  it 'should respond to methods' do
+    expect(subject).to respond_to(:rows, :cols, :grid)
+  end
 
   it 'begins with a 20x20 world' do
     expect(subject.grid).to eq(Array.new(20){ Array.new(20, 1) })
