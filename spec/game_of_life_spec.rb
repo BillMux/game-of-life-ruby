2fb4346ccdf178563b619ed1916bb4b9ac47b40d
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-02-26 14:21:04 +0000

    Refactor: inline 2d array

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -3,20 +3,7 @@ require_relative '../lib/game_of_life.rb'
 
 describe GameOfLife do
 
-  it 'begins with a 10x10 world' do
-    expect(subject.world).to eq(
-      [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-      ]
-    )
+  it 'begins with a 20x20 world' do
+    expect(subject.world).to eq(Array.new(20){ Array.new(20, 1) })
   end
 end
