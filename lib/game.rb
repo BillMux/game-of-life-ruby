c34b6e304fff07c8891887978bbcc34fdb1cbff1
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-01 17:04:19 +0000

    Add world and seed attrs

--- lib/game.rb
+++ lib/game.rb
@@ -2,5 +2,10 @@ require_relative 'cell'
 require_relative 'world'
 
 class Game
+  attr_reader :world, :seeds
 
+  def initialize(world = World.new(20, 20), seeds = [])
+    @world = world
+    @seeds = seeds
+  end
 end
