828f05e8f48b9a5b421c8de9a999b4decb936a1f
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-01 15:03:13 +0000

    Subject responds to coordinate attrs

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -7,4 +7,9 @@ describe Cell do
   it 'is dead by default' do
     expect(subject.alive).to be false
   end
+
+  it 'has x and y coordinates' do
+    expect(subject).to respond_to :x_position
+    expect(subject).to respond_to :y_position
+  end
 end
