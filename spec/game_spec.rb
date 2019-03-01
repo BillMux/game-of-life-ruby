93022da9fa8d97ac5c7251570347f6951550a9b4
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-01 17:06:21 +0000

    Test initialisation and response

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,34 @@
 require 'rspec'
+require_relative '../lib/cell.rb'
 require_relative '../lib/game.rb'
+require_relative '../lib/world.rb'
 
 describe Game do
-  subject { Game.new }
+  let(:world) { World.new(3, 3) }
+  let(:subject_cell) { Cell.new(1, 1) }
+  subject { Game.new(world, []) }
 
-  context 'when cell is alive' do
-    it 'dies if less than two live neighbours' do
-      
-    end
+  it 'initialises with the world object' do
+    expect(subject.world).to be_an_instance_of World
   end
 
+  it 'initialises with seeds' do
+    expect(subject.seeds).to be_an_instance_of Array
+    # expect(subject.seeds[0]).to be_an_instance_of Cell
+  end
+
+  it 'should respond to methods' do
+    expect(subject).to respond_to :world
+    expect(subject).to respond_to :seeds
+  end
+
+  context 'when cell is alive' do
+    before(:each) do
+      subject_cell.alive = true
+    end
+
+    # it 'dies if less than two live neighbours' do
+    #   expect(subject_cell.alive).to eq false
+    # end
+  end
 end
