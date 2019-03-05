933f3fe08dc4c76b294bce4d23d0bc0d1ccdcfaf
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-05 13:27:49 +0000

    Include alive? method

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -4,9 +4,11 @@ require_relative '../lib/cell.rb'
 describe Cell do
   subject { Cell.new(0, 0) }
   it { is_expected.to respond_to :alive }
+  it { is_expected.to respond_to :alive? }
 
   it 'is dead by default' do
     expect(subject.alive).to be false
+    expect(subject).not_to be_alive
   end
 
   it 'has x and y coordinates' do
