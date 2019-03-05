561eacd6f0337651372f650a4cc7b84621ef2a66
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-05 14:51:24 +0000

    Plant seeds on init, cells come to life

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,19 @@
 require_relative 'cell'
 require_relative 'world'
 
-# the rules and logic to run the Game of Life
+# contains rules and logic to run the Game of Life
 class Game
   attr_reader :world, :seeds
 
   def initialize(world = World.new(20, 20), seeds = [])
     @world = world
     @seeds = seeds
-    # plant_seeds
+    plant_seeds
   end
 
   def plant_seeds
-    @world.grid[seeds[0][0]][seeds[0][1]].alive = true
+    @seeds.each do |seed|
+      @world.grid[seed[0]][seed[1]].alive = true
+    end
   end
-
 end
