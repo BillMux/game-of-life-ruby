9c7cd8ba9fa1e9226292fea0a35df61e1412af37
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-05 19:02:10 +0000

    Add cell neighbours to init

--- lib/cell.rb
+++ lib/cell.rb
@@ -2,14 +2,14 @@
 
 # the cells within world, which can be dead or alive
 class Cell
-  attr_accessor :alive
-  attr_reader :x_coord, :y_coord, :neighbour_coords
+  attr_accessor :alive, :neighbours
+  attr_reader :x_coord, :y_coord
 
   def initialize(x_coord, y_coord)
     @alive = false
     @x_coord = x_coord
     @y_coord = y_coord
-    @neighbour_coords = [
+    @neighbours = [
       [x_coord - 1, y_coord - 1],
       [x_coord, y_coord - 1],
       [x_coord + 1, y_coord - 1],
