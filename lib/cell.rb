873f97f13545dde7112985714ea4c3dfec97ff60
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-05 18:47:43 +0000

    Introduce neighbours

--- lib/cell.rb
+++ lib/cell.rb
@@ -3,12 +3,22 @@
 # the cells within world, which can be dead or alive
 class Cell
   attr_accessor :alive
-  attr_reader :x_coord, :y_coord
+  attr_reader :x_coord, :y_coord, :neighbour_coords
 
   def initialize(x_coord, y_coord)
     @alive = false
     @x_coord = x_coord
     @y_coord = y_coord
+    @neighbour_coords = [
+      [x_coord - 1, y_coord - 1],
+      [x_coord, y_coord - 1],
+      [x_coord + 1, y_coord - 1],
+      [x_coord - 1, y_coord],
+      [x_coord + 1, y_coord],
+      [x_coord - 1, y_coord + 1],
+      [x_coord, y_coord + 1],
+      [x_coord + 1, y_coord + 1],
+    ]
   end
 
   def alive?
