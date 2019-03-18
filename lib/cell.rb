4932bcf32a38650c906c7d02ada3bfcf196426e6
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-18 09:29:04 +0000

    Inject world dimensions to init

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
+require_relative 'world'
+
 # the cells within world, which can be dead or alive
 class Cell
   attr_accessor :alive, :neighbours
-  attr_reader :x_coord, :y_coord
+  attr_reader :x_coord, :y_coord, :grid_width, :grid_length
 
-  def initialize(x_coord, y_coord)
+  def initialize(x_coord, y_coord, grid_width, grid_length)
     @alive = false
     @x_coord = x_coord
     @y_coord = y_coord
+    @grid_width = grid_width
+    @grid_length = grid_length
+
     @neighbours = [
       [x_coord - 1, y_coord - 1], [x_coord, y_coord - 1],
       [x_coord + 1, y_coord - 1], [x_coord - 1, y_coord],
@@ -26,7 +31,14 @@ class Cell
 
   def delete_edge_neighbours
     @neighbours.each do |coord|
-      coord.each { |x| @neighbours.delete(coord) if x.negative? }
+      if coord[0] >= @grid_width || coord[1] >= @grid_length
+        @neighbours.delete(coord)
+      end
+      coord.each do |x|
+        if x.negative?
+          @neighbours.delete(coord)
+        end
+      end
     end
-  end  
+  end
 end
