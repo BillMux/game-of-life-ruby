b1e45e72de935671ee559a28799175b5e82939cb
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-18 14:56:16 +0000

    Add response spec for grid dimensions

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -8,18 +8,15 @@ describe Cell do
   it { is_expected.to respond_to :alive, :alive?, :width, :length }
 
   it 'is dead by default' do
-    expect(subject.alive).to be false
     expect(subject).not_to be_alive
   end
 
   it 'has x and y coordinates' do
-    expect(subject.x_coord).to eq 1
-    expect(subject.y_coord).to eq 1
+    expect(subject.x_coord && subject.y_coord).to eq 1
   end
 
   it 'knows the size of its world' do
-    expect(subject.width).to eq 3
-    expect(subject.length).to eq 3
+    expect(subject.width && subject.length).to eq 3
   end
 
   it 'has neighbours, each with their own coordinates' do
@@ -33,10 +30,5 @@ describe Cell do
     subject3 = Cell.new(2, 1, 3, 3)
     expect(subject2.neighbours).to eq([[0, 0], [1, 0], [1, 1], [0, 2], [1, 2]])
     expect(subject3.neighbours).to eq([[1, 0], [2, 0], [1, 1], [1, 2], [2, 2]])
-  end
-
-  it 'knows dimensions of world' do
-    expect(subject.width).to eq 3
-    expect(subject.length).to eq 3
   end
 end
