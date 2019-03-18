dac6509350154c5717cf4760a081de504fe1d6b9
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-18 09:30:39 +0000

    Update cell declaration, edge neighbour test

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -5,10 +5,9 @@ require_relative '../lib/cell.rb'
 require_relative '../lib/world.rb'
 
 describe Cell do
-  subject { Cell.new(1, 1) }
-  let(:world) { World.new(3, 3) }
+  subject { Cell.new(1, 1, 3, 3) }
   it { is_expected.to respond_to :x_coord, :y_coord, :neighbours }
-  it { is_expected.to respond_to :alive, :alive? }
+  it { is_expected.to respond_to :alive, :alive?, :grid_width, :grid_length }
 
   it 'is dead by default' do
     expect(subject.alive).to be false
@@ -27,9 +26,14 @@ describe Cell do
   end
 
   it 'does not have neighbours on edge sides' do
-    subject2 = world.grid[0][1]
-    expect(subject2.neighbours).to eq(
-      [[0, 0], [1, 0], [1, 1], [0, 2], [1, 2]]
-    )
+    subject2 = Cell.new(0, 1, 3, 3)
+    subject3 = Cell.new(2, 1, 3, 3)
+    expect(subject2.neighbours).to eq([[0, 0], [1, 0], [1, 1], [0, 2], [1, 2]])
+    expect(subject3.neighbours).to eq([[1, 0], [2, 0], [1, 1], [1, 2], [2, 2]])
+  end
+
+  it 'knows dimensions of world' do
+    expect(subject.grid_width).to eq 3
+    expect(subject.grid_length).to eq 3
   end
 end
