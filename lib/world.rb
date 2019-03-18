3cca91156916920c5bae5d343be2f0664d383bde
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-18 09:29:52 +0000

    Declares cell with world dimensions

--- lib/world.rb
+++ lib/world.rb
@@ -4,7 +4,8 @@ require_relative 'cell'
 
 # 2d world in which the Game takes place
 class World
-  attr_reader :rows, :cols, :grid
+  attr_reader :rows, :cols
+  attr_accessor :grid
 
   def initialize(rows, cols)
     @rows = rows
@@ -12,15 +13,17 @@ class World
     @grid = create_grid
   end
 
-  def create_grid
-    Array.new(rows) do |row|
-      Array.new(cols) { |col| Cell.new(row, col) }
-    end
-  end
-
-  def find_live_neighbours(cell)
+  def count_live_neighbours(cell)
     live_neighbours = 0
     cell.neighbours.each { |x, y| live_neighbours += 1 if @grid[x][y].alive? }
     live_neighbours
+  end
+
+  private
+
+  def create_grid
+    Array.new(rows) do |row|
+      Array.new(cols) { |col| Cell.new(row, col, @rows, @cols) }
+    end
   end
 end
