a76b53389794b2e182c189b5966efa8b44d429b2
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-29 14:15:16 +0000

    Uncount cells with negative coords

--- spec/world_detect_neighbours_spec.rb
+++ spec/world_detect_neighbours_spec.rb
@@ -3,33 +3,30 @@
 describe World do
   subject { World.new(3, 3) }
 
-  it 'knows where to find cell neighbours' do
-    expect(subject.find_live_neighbours(1, 1)).to include(
-      [0, 0], [0, 1], [0, 2], [1, 0], [1, 2], [2, 0], [2, 1], [2, 2]
-    )
-  end
-
-  context 'counts cells live neighbours' do
+  context 'finds cells neighbours' do
     before(:each) do
       subject.grid[1][1].alive = true
-    end
-
-    it 'to the north, south, east and west' do
       subject.grid[0][1].alive = true
       subject.grid[2][1].alive = true
       subject.grid[1][2].alive = true
       subject.grid[1][0].alive = true
-    end
-
-    it 'diagonally' do
       subject.grid[0][0].alive = true
       subject.grid[0][2].alive = true
       subject.grid[2][0].alive = true
       subject.grid[2][2].alive = true
     end
+    it 'searching in the right places' do
+      expect(subject.find_neighbours(1, 1)).to include(
+        [0, 0], [0, 1], [0, 2], [1, 0], [1, 2], [2, 0], [2, 1], [2, 2]
+      )
+    end
 
-    after(:each) do
-      expect(subject.count_live_neighbours(1, 1)).to eq 4
+    it 'in all directions' do
+      expect(subject.count_live_neighbours(1, 1)).to eq 8
+    end
+
+    it 'does not count neighbours with negative coordinates' do
+      expect(subject.count_live_neighbours(0, 0)).to eq 3
     end
   end
 end
