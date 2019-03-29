c99a777397a80056923772313e5542da1fb1d2ac
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-29 16:28:55 +0000

    Test corner cell neighbours

--- spec/world_detect_neighbours_spec.rb
+++ spec/world_detect_neighbours_spec.rb
@@ -1,22 +1,21 @@
 # frozen_string_literal: true
 
+require_relative '../lib/world.rb'
+
 describe World do
   subject { World.new(3, 3) }
 
   context 'finds cells neighbours' do
     before(:each) do
-      subject.grid[1][1].alive = true
-      subject.grid[0][1].alive = true
-      subject.grid[2][1].alive = true
-      subject.grid[1][2].alive = true
-      subject.grid[1][0].alive = true
-      subject.grid[0][0].alive = true
-      subject.grid[0][2].alive = true
-      subject.grid[2][0].alive = true
-      subject.grid[2][2].alive = true
+      subject.grid.each do |row|
+        row.each do |col|
+          col.alive = true
+        end
+      end
     end
+
     it 'searching in the right places' do
-      expect(subject.find_neighbours(1, 1)).to include(
+      expect(subject.neighbours_of(1, 1)).to include(
         [0, 0], [0, 1], [0, 2], [1, 0], [1, 2], [2, 0], [2, 1], [2, 2]
       )
     end
@@ -27,6 +26,10 @@ describe World do
 
     it 'does not count neighbours with negative coordinates' do
       expect(subject.count_live_neighbours(0, 0)).to eq 3
+    end
+
+    it 'does not count neighbours with higher coordinates than grid size' do
+      expect(subject.count_live_neighbours(2, 2)).to eq 3
     end
   end
 end
