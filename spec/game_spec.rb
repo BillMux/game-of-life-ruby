b4c215bb1e22b2183f3dfb839b25fa54ddb9956a
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-03-29 23:15:25 +0000

    New spec for game rules

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -21,27 +21,4 @@ describe Game do
     expect(subject.world.grid[0][1]).to be_alive
     expect(subject.world.grid[0][2]).not_to be_alive
   end
-
-  context 'when cell is alive' do
-    it 'dies if less than two live neighbours' do
-      subject.tick!
-      expect(subject.world.grid[1][1]).not_to be_alive
-      expect(subject.world.grid[1][2]).not_to be_alive
-    end
-
-    it 'dies if more than three live neighbours' do
-      subject.seeds.push([2, 1], [0, 0])
-      expect(subject.world.grid[1][1]).to be_alive
-      subject.tick!
-      expect(subject.world.grid[1][1]).not_to be_alive
-    end
-  end
-
-  context 'when cell is dead, and has three live neighbours' do
-    it 'is brought back to life' do
-      subject.seeds.delete([1, 1])
-      subject.seeds.push([2, 1], [0, 0])
-      expect(subject.world.grid[1][1]).to be_alive
-    end
-  end
 end
---
+++ spec/rules_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative '../lib/game.rb'
+
+describe 'Game of Life rules' do
+  let(:world) { World.new(3, 3) }
+  subject { Game.new(world, [[1, 1], [0, 1]]) }
+
+  context 'when cell is alive' do
+    it 'dies if less than two live neighbours' do
+      subject.tick!
+      expect(subject.world.grid[1][1]).not_to be_alive
+      expect(subject.world.grid[1][2]).not_to be_alive
+    end
+
+    it 'dies if more than three live neighbours' do
+      subject.seeds.push([2, 1], [0, 0])
+      expect(subject.world.grid[1][1]).to be_alive
+      subject.tick!
+      expect(subject.world.grid[1][1]).not_to be_alive
+    end
+  end
+
+  context 'when cell is dead, and has three live neighbours' do
+    it 'is brought back to life' do
+      subject.seeds.delete([1, 1])
+      subject.seeds.push([2, 1], [0, 0])
+      subject.tick!
+      expect(subject.world.grid[1][1]).to be_alive
+    end
+  end
+end
