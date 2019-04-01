5280711a69e6b1f6c472a08437f81adf00882d63
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-04-01 19:24:29 +0100

    Test world population

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,7 +6,7 @@ describe Game do
   let(:world) { World.new(3, 3) }
   subject { Game.new(world, [[1, 1], [0, 1]]) }
 
-  it { is_expected.to respond_to :world, :seeds, :tick! }
+  it { is_expected.to respond_to :world, :seeds, :tick!, :randomly_populate }
 
   it 'initialises with the world object' do
     expect(subject.world).to be_an_instance_of World
@@ -20,5 +20,12 @@ describe Game do
     expect(subject.world.grid[1][1]).to be_alive
     expect(subject.world.grid[0][1]).to be_alive
     expect(subject.world.grid[0][2]).not_to be_alive
+  end
+
+  it 'randomly populates world' do
+    world = World.new(3, 3)
+    game = Game.new(world, [])
+    expect(game.seeds).to be_empty
+    expect(game.randomly_populate).not_to be_empty
   end
 end
