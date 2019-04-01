9f86b98bbe6098d8ae37e7cbd0bfa0b4f115d30b
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-04-01 17:51:46 +0100

    Split tick! into private methods, this satisfies rubocop

--- lib/game.rb
+++ lib/game.rb
@@ -9,32 +9,38 @@ class Game
   def initialize(world = World.new(20, 20), seeds = [])
     @world = world
     @seeds = seeds
+    @live_next_round = []
+    @dead_next_round = []
     plant_seeds
   end
 
   def tick!
-    live_next_round = []
-    dead_next_round = []
-
     @world.grid.each_with_index do |row, x|
       row.each_with_index do |cell, y|
-        live = @world.count_live_neighbours(x, y)
-        if (cell.alive? && (live < 2 || live > 3))
-          dead_next_round << cell
-        elsif !cell.alive? && live == 3
-          live_next_round << cell
-        else
-          cell.alive? ? live_next_round << cell : dead_next_round << cell
-        end
+        dead_or_alive(cell, @world.count_live_neighbours(x, y))
       end
     end
-    live_next_round.each { |cell| cell.alive = true }
-    dead_next_round.each { |cell| cell.alive = false }
+    @live_next_round.each { |cell| cell.alive = true }
+    @dead_next_round.each { |cell| cell.alive = false }
   end
 
   private
 
   def plant_seeds
     @seeds.each { |seed| @world.grid[seed[0]][seed[1]].alive = true }
+  end
+
+  def dead_or_alive(cell, live)
+    if cell.alive? && (live < 2 || live > 3)
+      @dead_next_round << cell
+    elsif !cell.alive? && live == 3
+      @live_next_round << cell
+    else
+      others_remain(cell)
+    end
+  end
+
+  def others_remain(cell)
+    cell.alive? ? @live_next_round << cell : @dead_next_round << cell
   end
 end
