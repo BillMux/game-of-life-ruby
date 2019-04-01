bcb48d3800de1b96c42621362afdb08d70fc4094
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-04-01 18:02:09 +0100

    Create GOL class for gosu interaction

---
+++ game_of_life.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'gosu'
+require_relative 'lib/game'
+
+class GameOfLife < Gosu::Window
+  def initialize
+
+  end
+
+  def update
+  end
+
+  def draw
+  end
+end
+
+GameOfLife.new.show
