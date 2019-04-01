5fa5762cd58e80349bd59de417d64bdfd52160c2
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-04-01 19:32:13 +0100

    Various linting

--- game_of_life.rb
+++ game_of_life.rb
@@ -3,25 +3,23 @@
 require 'gosu'
 require_relative 'lib/game'
 
+# The frontend controller, powered by Gosu
 class GameOfLife < Gosu::Window
   def initialize(width = 800, height = 600)
     @width = width
     @height = height
     @rows = @width / 10
     @cols = @height / 10
-    @world = World.new(@rows, @cols)
-    @game = Game.new(@world)
+    @game = Game.new(World.new(@rows, @cols))
     @game.randomly_populate!
 
     @background_colour = Gosu::Color.new(0xffdedede)
     @cell_colour = Gosu::Color.new.rgb(0, 0, 0)
-
     super width, height, false
     self.caption = "BillMux's Game of Life"
   end
 
-  def update
-  end
+  def update; end
 
   def draw
     draw_quad(0, 0, @background_colour,
