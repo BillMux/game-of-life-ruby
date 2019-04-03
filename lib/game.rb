93ac6e8c737ba33f7f99c9c06a66956944f333aa
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-04-03 15:58:34 +0100

    Create empty dead/alive arrs on tick

--- lib/game.rb
+++ lib/game.rb
@@ -9,12 +9,12 @@ class Game
   def initialize(world = World.new(20, 20), seeds = [])
     @world = world
     @seeds = seeds
-    @live_next_round = []
-    @dead_next_round = []
     plant_seeds
   end
 
   def tick!
+    @live_next_round = []
+    @dead_next_round = []
     @world.grid.each_with_index do |row, x|
       row.each_with_index do |cell, y|
         dead_or_alive(cell, @world.count_live_neighbours(x, y))
